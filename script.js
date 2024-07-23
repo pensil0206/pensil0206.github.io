b7d91bd16231aa3c139192de9aed7973998d1e12
paring <me@paring.moe>
2024-07-23 21:52:35 +0900

    refactor

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
+const translationData = [
+  [/해주세요/g, "돈주세요"],
+  // 추가 예시: [/안녕하세요/g, '펜바실보']
+];
+
+const getTranslatedText = (v) => {
+  let text = v;
+  for (const [from, to] of translationData) {
+    text = text.replace(from, to);
+  }
+
+  return text;
+};
+
 // Function to handle input submission
-function handleSubmit() {
-  // Get the value of the input field with id="userInput"
-  var inputVal = document.getElementById("userInput").value;
-
-  // Regular expression to find occurrences of "해주세요"
-  var regex = /해주세요/g;
-
-  // Count the number of occurrences
-  var matches = inputVal.match(regex);
-
-  // Determine the number of matches
-  var matchCount = matches ? matches.length : 0;
-
-  // Generate the appropriate display text based on the number of matches
-  var displayText =
-    matchCount > 0 ? "돈주세요\n".repeat(matchCount).trim() : inputVal;
-
-  // Set the display text
-  document.getElementById("displayArea").innerText = displayText;
-
-  // Clear the input field
-  document.getElementById("userInput").value = "";
+function translate(value) {
+  document.querySelector("#displayArea").value = getTranslatedText(value);
 }
 
-// Add event listener for the submit button
-document.getElementById("submitButton").addEventListener("click", handleSubmit);
+const input = document.querySelector("#userInput");
 
-// Add event listener for the input field to detect "Enter" key press
-document
-  .getElementById("userInput")
-  .addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault(); // Prevent form submission if the input is inside a form
-      handleSubmit();
-    }
-  });
+input.addEventListener(
+  "input",
+  (e) => {
+    translate(input.value);
+  },
+  false
+);
